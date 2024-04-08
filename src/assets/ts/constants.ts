13565e33330a74a41ec4820d641275ba59be9bd0
theskyworld <2456450687@qq.com>
2024-04-08 15:57:41 +0800

    feat: add LoginPage

--- src/assets/ts/constants.ts
+++ src/assets/ts/constants.ts
@@ -1,1 +1,8 @@
 export const LOGIN_URL = "/login";
+export const LOGIN = "登录";
+export const REGISTER = "注册";
+export const PHONE_NUMBER = "手机号";
+export const INPUT_YOUR_PHONE_NUMBER = "请输入手机号";
+export const PASSWORD = "密码";
+export const INPUT_YOUR_PASSWORD = "请输入密码";
+export const AGREE_ON_PRIVACY = "*登录即表示您赞同使用条款及隐私政策";
--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,3 +1,41 @@
+import "./loginPage.scss";
+import {
+  AGREE_ON_PRIVACY,
+  INPUT_YOUR_PASSWORD,
+  INPUT_YOUR_PHONE_NUMBER,
+  LOGIN,
+  PASSWORD,
+  PHONE_NUMBER,
+  REGISTER,
+} from "../../assets/ts/constants";
+
 export default function LoginPage() {
-  return <div>Login</div>;
+  return (
+    <div className="page login-page">
+      <div className="tab">
+        <div className="tab_item tab-item-left">{LOGIN}</div>
+        <div className="tab_item tab-item-right">{REGISTER}</div>
+      </div>
+      <div className="form">
+        <div className="form-item">
+          <div className="form-item-title">{PHONE_NUMBER}</div>
+          <input
+            type="text"
+            className="form-item-content"
+            placeholder={INPUT_YOUR_PHONE_NUMBER}
+          />
+        </div>
+        <div className="form-item">
+          <div className="form-item-title">{PASSWORD}</div>
+          <input
+            type="password"
+            className="form-item-content"
+            placeholder={INPUT_YOUR_PASSWORD}
+          />
+        </div>
+      </div>
+      <div className="submit">{LOGIN}</div>
+      <div className="notice">{AGREE_ON_PRIVACY}</div>
+    </div>
+  );
 }
